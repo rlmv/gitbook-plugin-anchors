a7974d3c608543a5b2f0c5e63449daaf4c8d42bf
Bo Marchman <bo.marchman@gmail.com>
2014-06-26 02:06:46 -0400

    remove html hooks

--- index.js
+++ index.js
@@ -9,22 +9,8 @@ module.exports = {
             "test.js"
         ],
         css: [
-            "test.css"
+            "plugin.css"
         ],
-        html: {
-            "html:start": function() {
-                return "<!-- Start book "+this.options.title+" -->"
-            },
-            "html:end": function() {
-                return "<!-- End of book "+this.options.title+" -->"
-            },
-
-            "head:start": "<!-- head:start -->",
-            "head:end": "<!-- head:end -->",
-
-            "body:start": "<!-- body:start -->",
-            "body:end": "<!-- body:end -->"
-        }
     },
     hooks: {
 
