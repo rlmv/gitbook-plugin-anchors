e9bdcc75f3f32782ea1ecad808da938dbcca2eed
Bo Marchman <bo.marchman@gmail.com>
2014-06-26 10:54:26 -0400

    refactor

--- index.js
+++ index.js
@@ -2,34 +2,36 @@
 var cheerio = require('cheerio');
 var _ = require('underscore');
 
+// insert anchor link into section
+var insertAnchors = function(section) {
+    
+    var $ = cheerio.load(section.content);
+
+    $(':header').each(function(i, elem) {
+	var header = $(elem);
+	var id = header.attr('id');
+	header.prepend('<a name="' + id + '" class="anchor" ' 
+		       + 'href="#' + id + '">' 
+		       + '<span class="fa fa-link"></span>'
+		       + '</a>');
+    });
+    section.content = $.html();
+};
+
 module.exports = {
     book: {
         assets: "./book",
         css: [ "plugin.css" ]
     },
     hooks: {
-
-        // Before html generation
-        "page": function(page) {
+        "page": function(page) { // before html generation
 	    
 	    sections = _.select(page.sections, function(section) {
 		return section.type == 'normal';
 	    }); // pluck all normal sections -- are they ever not normal?
 
-	    _.forEach(sections, function(section) {
-	
-		var $ = cheerio.load(section.content);
-		$(':header').each(function(i, elem) {
-		    var header = $(elem);
-		    var id = header.attr('id');
-		    header.prepend('<a name="' + id + '" class="anchor" ' 
-				   + 'href="#' + id + '">' 
-				   + '<span class="fa fa-link"></span>'
-				   + '</a>');
-		});
-		section.content = $.html();
-	    });
-	    
+	    _.forEach(sections, insertAnchors);
+
             return page;
         }
     }
