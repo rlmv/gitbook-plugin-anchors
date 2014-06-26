a0109ff60231bfa4230d7d4d1a964ae373d58715
Bo Marchman <bo.marchman@gmail.com>
2014-06-26 00:47:26 -0400

    rm unused hooks

--- index.js
+++ index.js
@@ -27,32 +27,6 @@ module.exports = {
         }
     },
     hooks: {
-        // For all the hooks, this represent the current generator
-
-        // This is called before the book is generated
-        "init": function() {
-            console.log("init!");
-        },
-
-        // This is called after the book generation
-        "finish": function() {
-            console.log("finish!");
-        },
-
-        // The following hooks are called for each page of the book
-        // and can be used to change page content (html, data or markdown)
-
-
-        // Before parsing markdown
-        "page:before": function(page) {
-            // page.path is the path to the file
-            // page.content is a string with the file markdown content
-
-            // Example:
-            //page.content = "# Title\n" + page.content;
-
-            return page;
-        },
 
         // Before html generation
         "page": function(page) {
@@ -76,7 +50,7 @@ module.exports = {
 	    });
 	    
             return page;
-        },
+        }
 /*  var escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
 
   return '<h' + level + '>'
@@ -87,16 +61,5 @@ module.exports = {
     + text
     + '</h' + level + '>';
 };*/
-
-        // After html generation
-        "page:after": function(page) {
-            // page.path is the path to the file
-            // page.content is a string with the html output
-
-            // Example:
-            //page.content = "<h1>Title</h1>\n" + page.content;
-            // -> This title will be added before the html tag so not visible in the browser
-            return page;
-        }
     }
 };
